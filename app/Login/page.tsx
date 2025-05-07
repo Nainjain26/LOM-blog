448b47fb579452258a3450c3163b0980e8940475
Nainjain26 <naina.jain2606@gmail.com>
2025-05-07 11:05:02 +0530

    update

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -99,6 +99,7 @@ const Login = () => {
                 type="email"
                 name="email"
                 placeholder="Email Address"
+                autoComplete="email"
                 onChange={handleChange}
                 required
                 className="w-full pl-10 p-3 border-2 border-black rounded-lg focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 transition-all"
@@ -114,6 +115,7 @@ const Login = () => {
                 type="password"
                 name="password"
                 placeholder="Password"
+                 autoComplete="current-password"
                 onChange={handleChange}
                 required
                 className="w-full pl-10 p-3 border-2 border-black rounded-lg focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 transition-all"
